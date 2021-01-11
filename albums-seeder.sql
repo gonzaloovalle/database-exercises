248800adaf8122b67666b21383f6fde6117cba66
Gonzalo Ovalle <gonzalo.ovalle01@gmail.com>
2021-01-11 17:02:07 -0600

    first commit

---
+++ albums-seeder.sql
@@ -0,0 +1,9 @@
+USE codup_test_db;
+
+INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES (Michael Jackson, Thriller, 1982, 66, Pop, Post-Disco, Funk, Rock),
+       (AC/DC, Back in Black, 1980, 50, Hard Rock),
+       (Meat Loaf, Bat Out of Hell, 1977, 50, Hard Rock, Glam Rock, Progressive Rock),
+       (Pink Flyod, The Dark Side of The Moon, 1973, 45, Progressive Rock),
+
+
