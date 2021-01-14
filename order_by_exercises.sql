5f527ca22187da90bc3dd31c7a49827382fb4050
Gonzalo Ovalle <gonzalo.ovalle01@gmail.com>
2021-01-14 09:17:59 -0600

    completed order by exercise

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -2,23 +2,31 @@ USE employees;
 
 SELECT *
 FROM employees
-ORDER BY first_name = 'Irene, Vidya, Maya';
+WHERE first_name IN ('Irene', 'Vidya', 'Maya')
+ORDER BY first_name;
 
+SELECT *
+FROM employees
+WHERE first_name IN ('Irene', 'Vidya', 'Maya')
+ORDER BY first_name, last_name;
+
+SELECT *
+FROM employees
+WHERE first_name IN ('Irene', 'Vidya', 'Maya')
+ORDER BY first_name, last_name;
 
 SELECT *
 FROM employees
 WHERE last_name LIKE 'E%'
-   OR '%e';
+ORDER BY emp_no;
 
 SELECT *
 FROM employees
-WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
+WHERE last_name LIKE 'E%'
+ORDER BY emp_no DESC;
 
 SELECT *
 FROM employees
-WHERE MONTH(birth_date) = '12'
-  AND DAY(birth_date) = '25';
-
-SELECT *
-FROM employees
-WHERE last_name LIKE '%q%';
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+AND birth_date LIKE '%-12-25'
+ORDER BY birth_date, hire_date DESC;
