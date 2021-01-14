431711d6c68feed1fad5e8773d605f7b15003e8d
Gonzalo Ovalle <gonzalo.ovalle01@gmail.com>
2021-01-14 09:22:19 -0600

    completed where exercises

--- where_exercises.sql
+++ where_exercises.sql
@@ -14,13 +14,10 @@ OR '%e';
 
 SELECT *
 FROM employees
-WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31';
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+AND birth_date LIKE '%-12-25';
 
 SELECT *
 FROM employees
-WHERE MONTH(birth_date) = '12'
-AND DAY(birth_date) = '25';
-
-SELECT *
-FROM employees
-WHERE last_name LIKE '%q%';
+WHERE last_name LIKE '%q%'
+AND last_name NOT LIKE '%qu%';
