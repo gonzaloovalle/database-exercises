46c2aafc68beea22521ceace4c689d19363fe436
Gonzalo Ovalle <gonzalo.ovalle01@gmail.com>
2021-01-23 18:54:44 -0600

    completed join and subqueries exercises

---
+++ subqueries_exercises.sql
@@ -0,0 +1,48 @@
+USE employees;
+
+SELECT first_name, last_name
+FROM employees
+WHERE hire_date IN (
+
+    SELECT hire_date
+    FROM employees
+    WHERE emp_no = 101010
+
+);
+
+SELECT title
+FROM titles
+WHERE emp_no IN (
+
+    SELECT emp_no
+    FROM employees
+    WHERE first_name = 'Aamod'
+
+);
+
+SELECT first_name, last_name
+FROM employees
+WHERE emp_no IN (
+
+    SELECT emp_no
+    FROM dept_manager
+    WHERE to_date > NOW()
+
+)
+  AND gender = 'F';
+
+SELECT dept_name
+FROM departments
+WHERE dept_no IN (
+
+    SELECT dept_no
+    FROM dept_manager
+    WHERE emp_no IN (
+
+        SELECT emp_no
+        FROM employees
+        WHERE gender = 'F'
+    )
+      AND to_date > NOW()
+
+);
